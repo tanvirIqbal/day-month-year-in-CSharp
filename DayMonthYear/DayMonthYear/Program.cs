68984f50b27ce503c34429e5187d4a562aa67d92
Tanvir Iqbal <tanviriqbal4u@live.com>
2020-09-23 15:52:19 +0600

    Day Month Year

---
+++ DayMonthYear/DayMonthYear/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace DayMonthYear
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime(2014, 12, 13, 0, 0, 0);
+            Console.WriteLine("Date1: {0}", date1);
+            DateTime date2 = new DateTime(2020, 8, 31, 0, 0, 0);
+            Console.WriteLine("Date2: {0}", date2);
+
+            DateDifference dateDifference = new DateDifference(date1, date2);
+            Console.WriteLine(dateDifference.ToString());
+            Console.ReadLine();
+        }
+    }
+
+
+    public class DateDifference
+    {
+        /// <summary>
+        /// defining Number of days in month; index 0=> january and 11=> December
+        /// february contain either 28 or 29 days, that's why here value is -1
+        /// which wil be calculate later.
+        /// </summary>
+        private int[] monthDay = new int[13] { 31, 31, -1, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        /// <summary>
+        /// contain from date
+        /// </summary>
+        private DateTime fromDate;
+
+        /// <summary>
+        /// contain To Date
+        /// </summary>
+        private DateTime toDate;
+
+        /// <summary>
+        /// this three variable for output representation..
+        /// </summary>
+        private int year;
+        private int month;
+        private int day;
+
+        public DateDifference(DateTime d1, DateTime d2)
+        {
+            int increment;
+
+            if (d1 > d2)
+            {
+                this.fromDate = d2;
+                this.toDate = d1.AddDays(1);
+            }
+            else
+            {
+                this.fromDate = d1;
+                this.toDate = d2.AddDays(1);
+            }
+
+            /// 
+            /// Day Calculation
+            /// 
+            increment = 0;
+
+            if (this.fromDate.Day > this.toDate.Day)
+            {
+                increment = this.monthDay[this.toDate.Month - 1];
+
+            }
+            /// if it is february month
+            /// if it's to day is less then from day
+            if (increment == -1)
+            {
+                if (DateTime.IsLeapYear(this.toDate.Year))
+                {
+                    // leap year february contain 29 days
+                    increment = 29;
+                }
+                else
+                {
+                    increment = 28;
+                }
+            }
+            if (increment != 0)
+            {
+                day = (this.toDate.Day + increment) - this.fromDate.Day;
+                increment = 1;
+            }
+            else
+            {
+                day = this.toDate.Day - this.fromDate.Day;
+            }
+
+            ///
+            ///month calculation
+            ///
+            if ((this.fromDate.Month + increment) > this.toDate.Month)
+            {
+                this.month = (this.toDate.Month + 12) - (this.fromDate.Month + increment);
+                increment = 1;
+            }
+            else
+            {
+                this.month = (this.toDate.Month) - (this.fromDate.Month + increment);
+                increment = 0;
+            }
+
+            ///
+            /// year calculation
+            ///
+            this.year = this.toDate.Year - (this.fromDate.Year + increment);
+
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return this.year + " Year(s), " + this.month + " month(s), " + this.day + " day(s)";
+        }
+
+        public int Years
+        {
+            get
+            {
+                return this.year;
+            }
+        }
+
+        public int Months
+        {
+            get
+            {
+                return this.month;
+            }
+        }
+
+        public int Days
+        {
+            get
+            {
+                return this.day;
+            }
+        }
+
+    }
+}
